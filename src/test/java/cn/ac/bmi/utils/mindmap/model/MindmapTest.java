caf3c73ab3a624dab2a2a1d3abc9f1ec103f3dfb
Huajian Mao <huajianmao@gmail.com>
2020-05-23 09:11:42 +0800

    Add several import rules for checkstyle

--- src/test/java/cn/ac/bmi/utils/mindmap/model/MindmapTest.java
+++ src/test/java/cn/ac/bmi/utils/mindmap/model/MindmapTest.java
@@ -1,14 +1,17 @@
 package cn.ac.bmi.utils.mindmap.model;
 
-import cn.ac.bmi.utils.mindmap.parser.MindmapParser;
-import cn.ac.bmi.utils.mindmap.parser.XmindParser;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class MindmapTest {
   @Test
   public void testObjectCreation() {
-    assertNotNull(new Mindmap(new Sheet[0], null,null));
+    Mindmap mindmap = new Mindmap(new Sheet[0], null,null);
+    assertNotNull(mindmap);
+    Sheet[] sheets = mindmap.getSheets();
+    assertNotNull(sheets);
+    assertEquals(sheets.length, 0);
   }
 }
